97cce622b9af6160087838c94967afbcf25c2e9c
gulyasm <mgulyas86@gmail.com>
2016-09-24 15:00:28 +0200

    Finished list, add, today, now

--- main.go
+++ main.go
@@ -4,19 +4,66 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/apcera/termtables"
 )
 
-func usage() {
-	fmt.Println("Commands: add, list")
+func usage(err string) {
+	fmt.Println("Commands: add, list, today")
+	fmt.Println("Error: " + err)
 }
 
 func exit(code int) {
 	os.Exit(code)
 }
 
+func cmdToday(fs FocusStore) error {
+	results, err := fs.ListToday()
+	if err != nil {
+		return err
+	}
+	printElements(results)
+	return nil
+}
+
+func cmdNow(fs FocusStore) error {
+	result, err := fs.Now()
+	if err != nil {
+		return err
+	}
+	fmt.Println(result.Name)
+	return nil
+}
+
+func cmdList(fs FocusStore) error {
+	results, err := fs.List()
+	if err != nil {
+		return err
+	}
+	printElements(results)
+	return nil
+}
+
+func printElements(elements []Element) {
+	table := termtables.CreateTable()
+	table.AddHeaders("Name", "Duration")
+	var td time.Duration
+
+	for _, r := range elements {
+		d := r.Duration()
+		table.AddRow(r.Name, d)
+		td += d
+	}
+	fmt.Println(strings.Repeat("=", 30))
+	fmt.Println("Total duration:", td)
+
+	fmt.Println(table.Render())
+}
 func main() {
 	if len(os.Args) < 2 {
-		usage()
+		usage("Missing cmd")
 		exit(1)
 	}
 	fs, err := NewFocusStore("test.db")
@@ -24,18 +71,22 @@ func main() {
 		log.Fatal(err)
 	}
 	cmd := os.Args[1]
-	var err error
 	switch cmd {
 	case "add":
-		err = fs.Add()
+		name := os.Args[2]
+		err = fs.Add(name)
 	case "list":
-		err = fs.List()
+		err = cmdList(fs)
+	case "today":
+		err = cmdToday(fs)
+	case "now":
+		err = cmdNow(fs)
 	default:
-		usage()
+		usage("Command not found: " + cmd)
 		exit(2)
 	}
 	if err != nil {
-		usage()
+		usage(err.Error())
 		exit(3)
 	}
 }
--- store.go
+++ store.go
@@ -2,44 +2,203 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"log"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type FocusStore struct {
-	db sql.DB
+const ISO8601 = "2006-01-02 15:04:05.000 MST"
+
+type Element struct {
+	Id    int
+	Name  string
+	Start time.Time
+	End   time.Time
 }
 
-func NewFocusStore(path string) (*FocusStore, error) {
-	db, err := sql.Open("sqlite3", path)
+func (e Element) Duration() time.Duration {
+	if e.End.IsZero() {
+		return time.Since(e.Start)
+	}
+	return e.End.Sub(e.Start)
+}
+
+type FocusStore struct {
+	path string
+}
+
+func NewFocusStore(path string) (FocusStore, error) {
+	fs := FocusStore{path}
+	err := fs.initDB()
+	return fs, err
+}
+
+func (fs FocusStore) initDB() error {
+	db, err := fs.open()
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer db.Close()
-
 	sqlStmt := `
-			CREATE TABLE Elements (
+			CREATE TABLE IF NOT EXISTS Elements (
 				Id INTEGER NOT NULL PRIMARY KEY, 
 				Name TEXT,
-				Start TEXT,
-				End TEXT
+				Start TEXT DEFAULT "",
+				End TEXT DEFAULT ""
 			);
 	`
 	_, err = db.Exec(sqlStmt)
+	return err
+}
+
+func (fs FocusStore) ListToday() ([]Element, error) {
+	result := []Element{}
+	db, err := fs.open()
 	if err != nil {
-		return nil, err
+		return result, err
+	}
+	defer db.Close()
+	today := time.Now().Format("2006-01-02")
+	rows, err := db.Query("SELECT Id, Name, Start, End FROM Elements WHERE Start > ?", today)
+	if err != nil {
+		return result, err
 	}
 
-	return &FocusStore{db}, err
+	for rows.Next() {
+		e := Element{}
+		var start, end string
+		if err := rows.Scan(
+			&e.Id,
+			&e.Name,
+			&start,
+			&end,
+		); err != nil {
+			return result, err
+		}
+		if start != "" {
+			e.Start, err = time.Parse(ISO8601, start)
+			if err != nil {
+				log.Println("Failed to parse time: ", err.Error())
+			}
+		}
+		if end != "" {
+			e.End, err = time.Parse(ISO8601, end)
+			if err != nil {
+				log.Println("Failed to parse time: ", err.Error())
+			}
+		}
+		result = append(result, e)
+
+	}
+
+	return result, nil
 }
 
-func (fs *FocusStore) List() error {
-	fmt.Println("List")
-	return nil
+func (fs FocusStore) Now() (Element, error) {
+	e := Element{}
+	db, err := fs.open()
+	if err != nil {
+		return e, err
+	}
+	defer db.Close()
+	var start string
+	row := db.QueryRow("SELECT Id, Name, Start FROM Elements WHERE End=''")
+	if err = row.Scan(&e.Id, &e.Name, &start); err != nil {
+		return e, err
+	}
+	if start != "" {
+		e.Start, err = time.Parse(ISO8601, start)
+		if err != nil {
+			log.Println("Failed to parse time: ", err.Error())
+		}
+	}
+	return e, nil
 }
 
-func (fs *FocusStore) Add() error {
-	fmt.Println("Add")
+func (fs FocusStore) List() ([]Element, error) {
+	result := []Element{}
+	db, err := fs.open()
+	if err != nil {
+		return result, err
+	}
+	defer db.Close()
+	rows, err := db.Query("SELECT Id, Name, Start, End FROM Elements")
+	if err != nil {
+		return result, err
+	}
+
+	for rows.Next() {
+		e := Element{}
+		var start, end string
+		if err := rows.Scan(
+			&e.Id,
+			&e.Name,
+			&start,
+			&end,
+		); err != nil {
+			return result, err
+		}
+		if start != "" {
+			e.Start, err = time.Parse(ISO8601, start)
+			if err != nil {
+				log.Println("Failed to parse time: ", err.Error())
+			}
+		}
+		if end != "" {
+			e.End, err = time.Parse(ISO8601, end)
+			if err != nil {
+				log.Println("Failed to parse time: ", err.Error())
+			}
+		}
+		result = append(result, e)
+
+	}
+
+	return result, nil
+}
+
+func (fs FocusStore) Add(name string) error {
+	db, err := fs.open()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	now := time.Now()
+
+	// Close previous activity
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec("UPDATE Elements SET End=? WHERE END=''", now.Format(ISO8601))
+	if err != nil {
+		log.Println("Failed to update DB.", err.Error())
+		err = tx.Rollback()
+		return err
+	}
+	// Start new activity
+	_, err = tx.Exec(
+		"INSERT INTO Elements(Name, Start) VALUES(?, ?)",
+		name,
+		now.Format(ISO8601))
+	if err != nil {
+		log.Println("Failed to update DB.", err.Error())
+		err = tx.Rollback()
+		return err
+	}
+
+	// Commit transaction
+	if err = tx.Commit(); err != nil {
+		log.Println("Failed to commit transaction", err.Error())
+		err = tx.Rollback()
+		return err
+	}
 	return nil
+
+}
+
+func (fs FocusStore) open() (*sql.DB, error) {
+	return sql.Open("sqlite3", fs.path)
 }
