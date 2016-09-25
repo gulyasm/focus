c5adc17f5872c5a95ef2b279513ed813d14d203b
gulyasm <mgulyas86@gmail.com>
2016-09-25 15:58:56 +0200

    Added Stop test

--- store_test.go
+++ store_test.go
@@ -7,6 +7,45 @@ import (
 	"testing"
 )
 
+func TestStop(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name())
+
+	s, err := NewFocusStore(tmpfile.Name())
+	if err != nil {
+		t.Error(err)
+	}
+	err = s.Start("Hello")
+	if err != nil {
+		t.Error(err)
+	}
+	r, err := s.List()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(r) != 1 {
+		t.Error(err)
+	}
+	if !r[0].IsRunning() {
+		t.Error("IsRunning returned false for an expected running element")
+	}
+	s.Stop()
+	r, err = s.List()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(r) != 1 {
+		t.Error(err)
+	}
+	if r[0].IsRunning() {
+		t.Error("IsRunning returned false for an expected running element")
+	}
+}
+
 func TestStart(t *testing.T) {
 	tmpfile, err := ioutil.TempFile("", "test.db")
 	if err != nil {
